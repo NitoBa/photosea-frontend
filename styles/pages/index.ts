aceaa76636d9b0ea20630835a8456ec84d57ddd1
Bruno Alves <bruno.santos@ifpi.edu.br>
2020-12-30 02:31:52 -0300

    added box shadow

--- styles/pages/index.ts
+++ styles/pages/index.ts
@@ -140,6 +140,7 @@ export const Container = styled.div`
           padding:22px;
           margin-bottom:20px;
           cursor: pointer;
+          box-shadow:0px 4px 4px rgba(0, 0, 0, 0.25);
 
           &:hover{
               color:white;
